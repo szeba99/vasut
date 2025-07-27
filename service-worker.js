00ad85bd215cd058d3842048823cadfa54c0c166
szeba99 <55421716+szeba99@users.noreply.github.com>
2025-07-27 15:56:19 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,16 +3,17 @@ self.addEventListener('install', (event) => {
     caches.open('soundboard-cache').then((cache) => {
       return cache.addAll([
         '/',
-        'szeba99.github.io/vasut/index.html',
-        'szeba99.github.io/vasut/style.css',
-        'szeba99.github.io/vasut/script.js',
-        'szeba99.github.io/vasut/sounds/*',
-        'szeba99.github.io/vasut/icons/icon-192x192.png',
-        'szeba99.github.io/vasut/icons/icon-512x512.png'
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/sounds/*',         // Hangfájlok
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png'
       ]);
     })
   );
 });
+
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
